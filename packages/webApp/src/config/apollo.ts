26d6649cca544ed410798237c632084950b6fa24
shaun Hutch <shaun@promoboxx.com>
2021-02-11 18:32:51 -0500

    Addrress (#16)
    * fix: remove dummy types
    
    * feat: secure api address

--- packages/webApp/src/config/apollo.ts
+++ packages/webApp/src/config/apollo.ts
@@ -1,9 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
-const uri =
-  process.env.NODE_ENV !== 'production'
-    ? 'http://localhost:4000'
-    : 'http://graphqlserver.eba-i2jkptfc.us-east-1.elasticbeanstalk.com/'
+const uri = process.env.NODE_ENV !== 'production' ? 'http://localhost:4000' : 'https://api.shaun-hutch.com/'
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),

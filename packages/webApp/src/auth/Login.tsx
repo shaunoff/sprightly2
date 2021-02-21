857d5561bfc8a914ab6511ec723325486bd96511
shaun Hutch <shaun@promoboxx.com>
2021-02-21 15:04:14 -0500

    feat: react router

--- packages/webApp/src/auth/Login.tsx
+++ packages/webApp/src/auth/Login.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import useAuth from './useAuth'
+import { Navigate } from 'react-router'
 
-const Login = () => {
+const Login: React.FC = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const { login } = useAuth()
+  const { login, isAuthenticated } = useAuth()
+  if (isAuthenticated) {
+    return <Navigate to={'/'} />
+  }
   return (
     <div>
       Login mun

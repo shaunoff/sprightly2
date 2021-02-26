0065779f62d339eccfdd39bf3d1a6891de53830b
shaun Hutch <shaun@promoboxx.com>
2021-02-25 22:39:03 -0500

    chore: tailwind

--- packages/webApp/src/App.tsx
+++ packages/webApp/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import ProtectedRoute from './components/ProtectedRoute'
 import { Routes, Route } from 'react-router-dom'
 import Login from './auth/Login'
